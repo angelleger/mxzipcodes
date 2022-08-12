533a1aeab22ec907b893c8e18050c6e6fa5f483a
angelleger <C-ALeger@nexstar.tv>
2022-08-11 20:06:18 -0400

    add controllers

---
+++ mxzipcodes-laravel/app/Http/Controllers/Api/ZipcodeController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\State;
+use App\Models\Zipcode;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+
+class ZipcodeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request, $zipcode)
+    {
+        $data = Zipcode::where('zipcode', $zipcode)->first();
+
+        // check if zipcode exist
+        if (!$data) {
+            return abort(404);
+        }
+
+        $cachedZipcode = Redis::get('zipcode_' . $zipcode);
+
+        if (isset($cachedZipcode)) {
+
+            $zipcode_response = json_decode($cachedZipcode, false);
+
+        } else {
+
+            $data->zipcode = str_pad($data->zipcode, 5, '0', STR_PAD_LEFT);
+
+            $zipcode_response = [
+
+                'zip_code' => "$data->zipcode",
+                'locality' => mb_strtoupper($data->locality->name ?? ""),
+                'federal_entity' => [
+                    'key' => $data->state->id,
+                    'name' => mb_strtoupper($data->state->name),
+                    'code' => $data->state->code,
+                ],
+                'settlements' =>
+                $data->settlementstest($data->id, $data->municipality_id, $data->zone_id)
+                ,
+                'municipality' => [
+                    'key' => $data->municipality->key,
+                    'name' => mb_strtoupper($data->municipality->name),
+                ],
+            ];
+
+            Redis::set('zipcode_' . $zipcode, json_encode($zipcode_response));
+
+        }
+
+        return response()->json($zipcode_response);
+    }
+
+}
---
+++ mxzipcodes-laravel/app/Http/Controllers/ZipcodeController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Imports\ZipcodesImport;
+use App\Models\Zipcode;
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+
+class ZipcodeController extends Controller
+{
+    /**
+     * @return \Illuminate\Support\Collection
+     */
+    public function index()
+    {
+        $zipcodes = Zipcode::get();
+
+        return view('zipcodes', compact('zipcodes'));
+    }
+
+
+    /**
+     * @return \Illuminate\Support\Collection
+     */
+    public function import()
+    {
+
+        // Excel::import(new ZoneImport, request()->file('file'));
+        // Excel::import(new MunicipalityImport, request()->file('file'));
+        // Excel::import(new SettlementtypesImport, request()->file('file'));
+        // Excel::import(new CitiesImport, request()->file('file'));
+        // Excel::import(new StatesImport, request()->file('file'));
+        // Excel::import(new SettlementsImport, request()->file('file'));
+        Excel::import(new ZipcodesImport, request()->file('file'));
+
+        return back();
+    }
+}
