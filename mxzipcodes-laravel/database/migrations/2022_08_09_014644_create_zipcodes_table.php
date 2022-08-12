dd69716e11b9e2319d39cf9a8fa99ce30143dbb0
angelleger <C-ALeger@nexstar.tv>
2022-08-11 20:04:49 -0400

    add migrations

---
+++ mxzipcodes-laravel/database/migrations/2022_08_09_014644_create_zipcodes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('zipcodes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('zipcode')->unique();
+            $table->unsignedBigInteger('locality_id')->nullable();
+            $table->foreign('locality_id')->references('id')->on('municipalities');
+            $table->unsignedBigInteger('municipality_id')->nullable();
+            $table->foreign('municipality_id')->references('id')->on('municipalities');
+            $table->unsignedBigInteger('state_id')->nullable();
+            $table->foreign('state_id')->references('id')->on('states');
+            $table->unsignedBigInteger('zone_id')->nullable();
+            $table->foreign('zone_id')->references('id')->on('zones');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('zipcodes');
+    }
+
+};
