40a8a3e14c6ecaee49f0122a65070e3b9f4f5bba
angelleger <C-ALeger@nexstar.tv>
2022-08-12 19:49:12 -0400

    Update import

--- app/Http/Controllers/ZipcodeController.php
+++ app/Http/Controllers/ZipcodeController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Imports\ZipcodesImport;
+use App\Imports\CitiesImport;
 use App\Imports\LocalitiesImport;
 use App\Imports\MunicipalityImport;
-use App\Imports\CitiesImport;
-use App\Imports\StatesImport;
 use App\Imports\SettlementsImport;
+use App\Imports\StatesImport;
+use App\Imports\ZipcodesImport;
 use App\Imports\ZoneImport;
 use App\Models\Zipcode;
 use Illuminate\Http\Request;
@@ -25,22 +25,21 @@ class ZipcodeController extends Controller
         return view('zipcodes', compact('zipcodes'));
     }
 
-
     /**
      * @return \Illuminate\Support\Collection
      */
     public function import()
     {
 
-        // Excel::import(new ZoneImport, request()->file('file'));
-       Excel::import(new LocalitiesImport, request()->file('file'));
-        // Excel::import(new MunicipalityImport, request()->file('file'));
-        // Excel::import(new SettlementtypesImport, request()->file('file'));
-        // Excel::import(new CitiesImport, request()->file('file'));
-        // Excel::import(new StatesImport, request()->file('file'));
-        // Excel::import(new SettlementsImport, request()->file('file'));
-         Excel::import(new ZipcodesImport, request()->file('file'));
+        Excel::import(new ZoneImport, request()->file('file'));
+        Excel::import(new LocalitiesImport, request()->file('file'));
+        Excel::import(new MunicipalityImport, request()->file('file'));
+        Excel::import(new SettlementtypesImport, request()->file('file'));
+        Excel::import(new CitiesImport, request()->file('file'));
+        Excel::import(new StatesImport, request()->file('file'));
+        Excel::import(new SettlementsImport, request()->file('file'));
+        Excel::import(new ZipcodesImport, request()->file('file'));
 
-       // return back();
+        return back();
     }
 }
