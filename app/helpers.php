e1c7c8d79148bff78cf139ae3983c9246de68194
angelleger <C-ALeger@nexstar.tv>
2022-08-13 18:21:50 -0400

    Update to replicate endpoint

--- app/helpers.php
+++ app/helpers.php
@@ -4,6 +4,9 @@
 if (!function_exists('remove_accents')) {
     function remove_accents($string)
     {
+        $string =str_replace('ñ', '?', $string);
+        $string = str_replace('Ñ', '?', $string);
+
         return iconv('UTF-8', 'ASCII//TRANSLIT', $string);
     }
 }
