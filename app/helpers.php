9fb09ee9283bfcf1a2e4a263ea180508ee44d26f
angelleger <C-ALeger@nexstar.tv>
2022-08-17 16:41:35 -0400

    Update remove_accents function

--- app/helpers.php
+++ app/helpers.php
@@ -4,12 +4,10 @@
 if (!function_exists('remove_accents')) {
     function remove_accents($string)
     {
-        // $string =str_replace('ñ', '?', $string);
-        // // $string = str_replace('Ñ', '?', $string);
-        // $string = str_replace('ü', '?', $string);
-        // // $string = str_replace('Ü', '?', $string);
-
-        // return iconv('UTF-8', 'ASCII//TRANSLIT', $string);
+        $string = str_replace('ñ', '?', $string);
+        $string = str_replace('Ñ', '?', $string);
+        $string = str_replace('ü', '?', $string);
+        $string = str_replace('Ü', '?', $string);
 
         return $string;
     }
